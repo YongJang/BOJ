5d4b933ea35c22b773b3a17e55c12fbc822e3e41
YongJang <yj_430@naver.com>
2016-12-14 00:26:50 +0900

    solve 2 problem

---
+++ src/BOJ10172.java
@@ -0,0 +1,13 @@
+
+/**
+ * Created by USER on 2016-12-14.
+ */
+public class BOJ10172 {
+    public static void main(String args[]) {
+        System.out.println("|\\_/|\n" +
+                "|q p|   /}\n" +
+                "( 0 )\"\"\"\\\n" +
+                "|\"^\"`    |\n" +
+                "||_/=\\\\__|");
+    }
+}
---
+++ src/BOJ10943.java
@@ -0,0 +1,8 @@
+/**
+ * Created by USER on 2016-12-14.
+ */
+public class BOJ10943 {
+    public static void main(String args[]) {
+        System.out.println((int)(Math.random() * 10) + 1);
+    }
+}
